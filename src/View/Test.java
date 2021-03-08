85cd7225223f6e1b8b204f4ce81d4f2490f329dc
nguyentran-se <nguyentranfw@gmail.com>
2021-03-08 20:23:14 +0700

    Beta v1.1

--- src/View/Test.java
+++ src/View/Test.java
@@ -4,9 +4,9 @@ import Util.*;
 public class Test {
 
     public static void main(String[] args)  throws Exception{
-        Normalize nm = new Normalize("Example/Input.txt");
+        Normalize nm = new Normalize("src/Example/Input.txt");
         nm.normailze();
-        nm.export("Example/Output.txt");
+        nm.export("src/Example/Output.txt");
 
         // String [] a = "
     }
