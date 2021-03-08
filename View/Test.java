553405c247aebc6d909370c137f30e64db42d556
nqbao11 <nqbao11@gmail.com>
2021-03-08 20:02:48 +0700

    Beta Version

---
+++ View/Test.java
@@ -0,0 +1,14 @@
+package View;
+import Util.*;
+
+public class Test {
+
+    public static void main(String[] args)  throws Exception{
+        Normalize nm = new Normalize("Example/Input.txt");
+        nm.normailze();
+        nm.export("Example/Output.txt");
+
+        // String [] a = "
+    }
+
+}
